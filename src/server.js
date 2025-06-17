3f7632242239497b7347fcb9ec9c8f8dfa584002
Ashraf Manna <optimisticashraf@gmail.com>
2025-06-17 14:46:44 +0600

    updating start point

--- src/server.js
+++ src/server.js
@@ -4,15 +4,11 @@ import { connectDB } from "./config/mongoose.js";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
 
 const bootstrap = async () => {
   await connectDB();
   app.listen(port, () => {
-    console.log(`🚀 Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
   });
 };
 
